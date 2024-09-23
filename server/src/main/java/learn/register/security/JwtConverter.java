a5c9c9f4a2764f54ddb988d39c69043e9b01cd2e
Michael Steidel <masteidel@gmail.com>
2024-09-23 12:46:24 -0400

    Add method to generate JWT from User object

--- server/src/main/java/learn/register/security/JwtConverter.java
+++ server/src/main/java/learn/register/security/JwtConverter.java
@@ -2,9 +2,12 @@ package learn.register.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Component
 public class JwtConverter {
@@ -14,4 +17,19 @@ public class JwtConverter {
     private final String ISSUER = "register";
     private final int EXPIRATION_MINUTES = 15;
     private final int EXPIRATION_MILLIS = EXPIRATION_MINUTES * 60 * 1000;
+
+    public String getTokenFromUser(User user) {
+
+        String authorities = user.getAuthorities().stream()
+                .map(i -> i.getAuthority())
+                .collect(Collectors.joining(","));
+
+        return Jwts.builder()
+                .setIssuer(ISSUER)
+                .setSubject(user.getUsername())
+                .claim("authorities", authorities)
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MILLIS))
+                .signWith(key)
+                .compact();
+    }
 }
