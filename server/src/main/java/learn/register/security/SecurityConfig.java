7db96236a6313b276229885eb39ea2c24b5486e6
Michael Steidel <masteidel@gmail.com>
2024-09-23 13:48:30 -0400

    Add JWT-based authentication and stateless session management

--- server/src/main/java/learn/register/security/SecurityConfig.java
+++ server/src/main/java/learn/register/security/SecurityConfig.java
@@ -1,6 +1,8 @@
 package learn.register.security;
 
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -8,5 +10,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     public SecurityConfig(JwtConverter converter) {
         this.converter = converter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.cors();
+
+        http.authorizeRequests()
+                .antMatchers("/api/user/authenticate").permitAll()
+                .antMatchers("/api/user/register").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new JwtRequestFilter(authenticationManager(), converter))
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
