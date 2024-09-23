f70f7822e652d6631c8c8b7d3c22f9e1182c91e2
Michael Steidel <masteidel@gmail.com>
2024-09-23 12:21:48 -0400

    Add key and expiration constants to JwtConverter

--- server/src/main/java/learn/register/security/JwtConverter.java
+++ server/src/main/java/learn/register/security/JwtConverter.java
@@ -1,4 +1,15 @@
 package learn.register.security;
 
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
+import java.security.Key;
+
 public class JwtConverter {
+
+    private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+    private final String ISSUER = "register";
+    private final int EXPIRATION_MINUTES = 15;
+    private final int EXPIRATION_MILLIS = EXPIRATION_MINUTES * 60 * 1000;
 }
