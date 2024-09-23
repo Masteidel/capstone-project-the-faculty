7f142d57b2292e616b0767cc0f803e429b870c47
Michael Steidel <masteidel@gmail.com>
2024-09-23 12:07:26 -0400

    Introduce JdbcTemplate dependency in AppUserRepository
    Added a JdbcTemplate member to the AppUserJdbcTemplateRepository class and initialized it through a constructor. This change is a prerequisite for future implementations involving database operations.

--- server/src/main/java/learn/register/data/AppUserJdbcTemplateRepository.java
+++ server/src/main/java/learn/register/data/AppUserJdbcTemplateRepository.java
@@ -1,8 +1,16 @@
 package learn.register.data;
 
 import learn.register.models.AppUser;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class AppUserJdbcTemplateRepository implements AppUserRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public AppUserJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public AppUser findByUsername(String username) {
         return null;
