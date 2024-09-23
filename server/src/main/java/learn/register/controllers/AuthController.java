b95803a53923a7abbddb5cf1c4d42ff10ceec5dd
Michael Steidel <masteidel@gmail.com>
2024-09-23 16:02:26 -0400

    Add REST annotations to AuthController

--- server/src/main/java/learn/register/controllers/AuthController.java
+++ server/src/main/java/learn/register/controllers/AuthController.java
@@ -1,4 +1,9 @@
 package learn.register.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
 public class AuthController {
 }
