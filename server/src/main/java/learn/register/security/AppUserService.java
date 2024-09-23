9849417f23419452bc55af9b16dac80e83e76612
Michael Steidel <masteidel@gmail.com>
2024-09-23 14:11:27 -0400

    Implement UserDetailsService in AppUserService

--- server/src/main/java/learn/register/security/AppUserService.java
+++ server/src/main/java/learn/register/security/AppUserService.java
@@ -1,4 +1,23 @@
 package learn.register.security;
 
-public class AppUserService {
+import learn.register.data.AppUserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AppUserService implements UserDetailsService {
+
+    private final AppUserRepository repository;
+    private final PasswordEncoder encoder;
+
+    public AppUserService(AppUserRepository repository, PasswordEncoder encoder) {
+        this.repository = repository;
+        this.encoder = encoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
+        return null;
+    }
 }
