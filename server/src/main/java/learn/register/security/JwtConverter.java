96a3888a9339ed8efba5e4727719dd49e8a2824c
Michael Steidel <masteidel@gmail.com>
2024-09-23 12:35:09 -0400

    Add @Component annotation to JwtConverter class

--- server/src/main/java/learn/register/security/JwtConverter.java
+++ server/src/main/java/learn/register/security/JwtConverter.java
@@ -2,9 +2,11 @@ package learn.register.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
 
+@Component
 public class JwtConverter {
 
     private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
