1f4926ab0d5b9b0cfce295bf3ed6679f36f40440
Michael Steidel <masteidel@gmail.com>
2024-09-23 13:21:22 -0400

    Extend SecurityConfig with WebSecurityConfigurerAdapter

--- server/src/main/java/learn/register/security/SecurityConfig.java
+++ server/src/main/java/learn/register/security/SecurityConfig.java
@@ -1,4 +1,12 @@
 package learn.register.security;
 
-public class SecurityConfig {
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtConverter converter;
+
+    public SecurityConfig(JwtConverter converter) {
+        this.converter = converter;
+    }
 }
