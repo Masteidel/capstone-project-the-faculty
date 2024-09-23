0eb3b7119fd08fc332db6f1ea578979cd8683015
Michael Steidel <masteidel@gmail.com>
2024-09-23 13:14:51 -0400

    Add method to extract User from JWT token

--- server/src/main/java/learn/register/security/JwtConverter.java
+++ server/src/main/java/learn/register/security/JwtConverter.java
@@ -2,11 +2,15 @@ package learn.register.security;
 
 import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 
 import java.security.Key;
+import java.util.Arrays;
 import java.util.Date;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @Component
@@ -31,5 +35,33 @@ public class JwtConverter {
                 .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_MILLIS))
                 .signWith(key)
                 .compact();
+    }
+
+    public User getUserFromToken(String token) {
+
+        if(token == null || !token.startsWith("Bearer ")) {
+            return null;
+        }
+
+        try {
+            Jws<Claims> jws = Jwts.parserBuilder()
+                    .requireIssuer(ISSUER)
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token.substring(7));
+
+            String username = jws.getBody().getSubject();
+            String authStr = (String) jws.getBody().get("authorities");
+            List<GrantedAuthority> authorities = Arrays.stream(authStr.split(","))
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+
+            return new User(username, username, authorities);
+
+        } catch (JwtException e) {
+            System.out.println(e);
+        }
+
+        return null;
     }
 }
