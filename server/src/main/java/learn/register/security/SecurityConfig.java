934753e28020a2b2564e47053cc7a6d56aff35d0
Michael Steidel <masteidel@gmail.com>
2024-09-23 13:54:28 -0400

    Enable AuthenticationManager Bean in SecurityConfig

--- server/src/main/java/learn/register/security/SecurityConfig.java
+++ server/src/main/java/learn/register/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package learn.register.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -26,5 +28,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilter(new JwtRequestFilter(authenticationManager(), converter))
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 }
