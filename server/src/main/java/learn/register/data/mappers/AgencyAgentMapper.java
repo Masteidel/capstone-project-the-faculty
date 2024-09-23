8e8b3af0338b1f582673b276ffb51539a6f535f7
Henry <henrywinczner@gmail.com>
2024-09-23 12:00:53 -0400

    added student controller

--- server/src/main/java/learn/register/data/mappers/AgencyAgentMapper.java
+++ server/src/main/java/learn/register/data/mappers/AgencyAgentMapper.java
@@ -11,18 +11,8 @@ public class AgencyAgentMapper implements RowMapper<Student> {
     @Override
     public Student mapRow(ResultSet resultSet, int i) throws SQLException {
 
-        Student agencyAgent = new Student();
-        agencyAgent.setAgencyId(resultSet.getInt("agency_id"));
-        agencyAgent.setIdentifier(resultSet.getString("identifier"));
-        agencyAgent.setActivationDate(resultSet.getDate("activation_date").toLocalDate());
-        agencyAgent.setActive(resultSet.getBoolean("is_active"));
 
-        SecurityClearanceMapper securityClearanceMapper = new SecurityClearanceMapper();
-        agencyAgent.setSecurityClearance(securityClearanceMapper.mapRow(resultSet, i));
-
-        AgentMapper agentMapper = new AgentMapper();
-        agencyAgent.setAgent(agentMapper.mapRow(resultSet, i));
-
+        Student agencyAgent;
         return agencyAgent;
     }
 }
