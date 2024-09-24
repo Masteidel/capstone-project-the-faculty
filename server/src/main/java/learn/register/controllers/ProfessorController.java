2b19cdab8d60574d7101f77b1fcc7635fe83f144
Michael Steidel <masteidel@gmail.com>
2024-09-24 11:03:52 -0400

    Fix ProfessorService import to the domain package

--- server/src/main/java/learn/register/controllers/ProfessorController.java
+++ server/src/main/java/learn/register/controllers/ProfessorController.java
@@ -1,7 +1,7 @@
 package learn.register.controllers;
 
 import learn.register.models.Professor;
-import learn.register.services.ProfessorService;
+import learn.register.domain.ProfessorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
