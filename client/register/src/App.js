3afbb7ead28a19e0957df9edb6dcd8347a0c0fb4
Michael Steidel <masteidel@gmail.com>
2024-09-24 14:29:02 -0400

    Add CourseList component and route

--- client/register/src/App.js
+++ client/register/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Intro from "./components/Intro";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
+import CourseList from "./components/CourseList";
 
 
 function App() {
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<Intro />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
+        <Route path="/courses" element={<CourseList />}></Route>
+        {/*<Route path="/course/:courseId" element={<Course />}></Route>*/}
       </Routes>
     </Router>
   );
