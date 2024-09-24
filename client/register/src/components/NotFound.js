9cfdfc86e32e39e758a39823c7b80dc4826b5115
Michael Steidel <masteidel@gmail.com>
2024-09-24 13:48:38 -0400

    Add initial NotFound component for handling 404 errors

---
+++ client/register/src/components/NotFound.js
@@ -0,0 +1,9 @@
+function NotFound() {
+    return (
+        <div className="image-container">
+            <img src="https://www.verticalrail.com/wp-content/uploads/2015/05/404-Page-Not-Found-150x150.png" alt="404 Not Found"/>
+        </div>
+    );
+}
+
+export default NotFound;
