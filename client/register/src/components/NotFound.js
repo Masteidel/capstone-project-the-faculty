3acdffbad208544a3ec28a831779de500738d405
Michael Steidel <masteidel@gmail.com>
2024-09-24 17:45:29 -0400

    Enhance NotFound component

--- client/register/src/components/NotFound.js
+++ client/register/src/components/NotFound.js
@@ -1,7 +1,20 @@
 function NotFound() {
     return (
-        <div className="image-container">
-            <img src="https://www.verticalrail.com/wp-content/uploads/2015/05/404-Page-Not-Found-150x150.png" alt="404 Not Found"/>
+        <div className="not-found-container">
+            <h1 className="not-found-title">404</h1>
+            <p className="not-found-subtitle">Oops! The page you're looking for doesn't exist.</p>
+            <p className="not-found-description">
+                It seems like the page you are trying to reach has been moved, deleted, or never existed. Here's some random fun instead!
+            </p>
+            <iframe
+                src="https://giphy.com/embed/l2JehQ2GitHGdVG9y"
+                width="480"
+                height="270"
+                frameBorder="0"
+                allowFullScreen
+                title="fun-gif">
+            </iframe>
+            <Link to="/" className="not-found-home-link">Go to Homepage</Link>
         </div>
     );
 }
