53b8f8de6574c6001a1c200ef6f80590117c1cf8
Michael Steidel <masteidel@gmail.com>
2024-09-24 14:32:17 -0400

    Add data fetching logic to CourseList component

--- client/register/src/components/CourseList.js
+++ client/register/src/components/CourseList.js
@@ -1,10 +1,27 @@
 import {Link} from "react-router-dom";
-import {useState} from "react";
+import {useEffect, useState} from "react";
 
 function CourseList() {
 
     // State Variables
     const [courses, setCourses] = useState([]);
+
+    const url = 'http://localhost:8080/api/register';
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => {
+                if (res.status === 200) {
+                    return res.json();
+                } else {
+                    return Promise.reject(`Unexpected Status Code: ${res.status}`);
+                }
+            })
+            .then(data => {
+                setCourses(data)
+            })
+            .catch(console.log);
+    }, []);
 
     return (
         <section>
