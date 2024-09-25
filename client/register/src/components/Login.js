8d78f4a993bcd6a5c9375f4435b2d2be583205d2
Miguel Galindo <miguelgalindo1214@gmail.com>
2024-09-25 12:26:50 -0400

    Added similar styling to login page and sign up page

--- client/register/src/components/Login.js
+++ client/register/src/components/Login.js
@@ -1,33 +1,44 @@
-function Login() {
+import React from 'react';
 
+function Login() {
     return (
-        <section className="container">
-            <form>
-                <fieldset className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input
-                        id="email"
-                        name="email"
-                        type="email"
-                        className="form-control"
-                    />
-                </fieldset>
-                <fieldset className="form-group">
-                    <label htmlFor="password">Password</label>
-                    <input
-                        id="password"
-                        name="password"
-                        type="password"
-                        className="form-control"
-                    />
-                </fieldset>
-                <div className="mt-4">
-                    <button type="submit" className="btn btn-dark">
-                        Log In
-                    </button>
+        <div className="container d-flex justify-content-center align-items-center vh-100">
+            <div className="card shadow-sm" style={{ width: '400px' }}>
+                <div className="card-body">
+                    <h3 className="text-center mb-4">Login</h3>
+                    <form>
+                        <fieldset className="form-group">
+                            <label htmlFor="email">Email</label>
+                            <input
+                                id="email"
+                                name="email"
+                                type="email"
+                                className="form-control"
+                                placeholder="Enter your email"
+                            />
+                        </fieldset>
+                        <fieldset className="form-group mt-3">
+                            <label htmlFor="password">Password</label>
+                            <input
+                                id="password"
+                                name="password"
+                                type="password"
+                                className="form-control"
+                                placeholder="Enter your password"
+                            />
+                        </fieldset>
+                        <div className="mt-4 d-grid">
+                            <button type="submit" className="btn btn-dark btn-block">
+                                Log In
+                            </button>
+                        </div>
+                        <div className="mt-3 text-center">
+                            <a href="#" className="text-decoration-none">Forgot Password?</a>
+                        </div>
+                    </form>
                 </div>
-            </form>
-        </section>
+            </div>
+        </div>
     );
 }
 
