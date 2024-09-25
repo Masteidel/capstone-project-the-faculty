6a0d946a1f3b40018469ebe2457c3c197a7ad714
Miguel Galindo <miguelgalindo1214@gmail.com>
2024-09-25 13:21:29 -0400

    Adding Home Route to App

--- client/register/src/App.js
+++ client/register/src/App.js
@@ -3,6 +3,8 @@ import Intro from "./components/Intro";
 import Signup from "./components/Signup";
 import Login from "./components/Login";
 import CourseList from "./components/CourseList";
+import CourseSections from "./CourseSections";
+import Home from "./components/Home";
 
 
 function App() {
@@ -10,10 +12,12 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Intro />}></Route>
+        <Route path="/home" element={<Home />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/login" element={<Login />}></Route>
         <Route path="/courses" element={<CourseList />}></Route>
         {/*<Route path="/course/:courseId" element={<Course />}></Route>*/}
+        <Route path="/course/:courseId/sections" element={<CourseSections />} />
       </Routes>
     </Router>
   );
