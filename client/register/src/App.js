7db2840c0e3aaf4be077f7844be3ad79778a6fce
Miguel Galindo <miguelgalindo1214@gmail.com>
2024-09-25 20:24:43 -0400

    Updating App to include Home route

--- client/register/src/App.js
+++ client/register/src/App.js
@@ -9,6 +9,7 @@ import StudentLogin from "./components/StudentLogin"; // Import Student Login co
 import SectionList from "./components/SectionList";
 import Enrollment from "./components/Enrollment";
 import StudentForm from "./components/StudentForm";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
           <Route path="sections" element={<SectionList />} />
           <Route path="enrollment" element={<Enrollment />} />
           <Route path="/student-form" element={<StudentForm />} />
+          <Route path="/home" element={<Home />} />
           {/* Add more routes as necessary */}
         </Route>
       </Routes>
